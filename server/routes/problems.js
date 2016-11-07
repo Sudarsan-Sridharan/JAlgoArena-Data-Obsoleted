b338291a53eb626cd0baa761ed1e8b50df9b7147
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-11-07 18:09:00 +0100

    - problems endpoint exposed directly for UI, hide test_cases and function

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -3,6 +3,7 @@ module.exports = function(app, problemsDb) {
     app.get('/problems', function(req, res, next) {
         problemsDb.find(
             {},
+            {test_cases: 0, function: 0},
             function (err, docs) {
                 if (err) return next(err);
                 return res.json(docs);
